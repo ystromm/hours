03eb47ba3d0572c408922a96792b296d93df0e7e
Martin Carlsson <martin.carlsson@transmode.se>
2015-12-27 17:52:47 +0100

    Added google oauth2 login

--- src/main/java/com/github/ystromm/hours/HoursMain.java
+++ src/main/java/com/github/ystromm/hours/HoursMain.java
@@ -2,13 +2,30 @@ package com.github.ystromm.hours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * The main class for hours.
  * Created by mac on 2015-12-21.
  */
-@EnableAutoConfiguration
-public class HoursMain {
+@EnableOAuth2Sso
+@SpringBootApplication
+public class HoursMain extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
     public static void main(String[] args) {
         SpringApplication.run(HoursMain.class, args);
     }
--- src/main/java/com/github/ystromm/hours/UserController.java
+++ src/main/java/com/github/ystromm/hours/UserController.java
@@ -1,7 +1,17 @@
 package com.github.ystromm.hours;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
 /**
- * Created by mac on 2015-12-27.
+ * Controller for getting the current user.
  */
+@RestController
 public class UserController {
+    @RequestMapping("/api/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 }
